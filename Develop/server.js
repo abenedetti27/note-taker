160d4f7bb265c1cc790fe9d98c31d114e20ed069
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-06 12:22:10 -0800

    Add routes for retrieving and creating notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const { get } = require('http');
 const path = require('path');
 
 const app = express();
@@ -9,4 +10,18 @@ app.use(express.json());
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    const notes = getNotes();
+    res.json(notes);
+});
+
+app.post('/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = generateUniqueId();
+    const notes = getNotes();
+    notes.push(newNote);
+    saveNotes(notes);
+    res.json(newNote);
 });
