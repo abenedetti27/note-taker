6ceb871ae0c1c6d18b3eb3723c246707d97d8a74
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-06 21:42:12 -0800

    Add express static middleware for serving static files

--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
